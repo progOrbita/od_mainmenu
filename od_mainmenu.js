4a085d95782d141a790e0717285edaa8656312c1
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-28 18:26:32 +0200

    removed when over an element

--- od_mainmenu.js
+++ od_mainmenu.js
@@ -9,10 +9,11 @@ $(function () {
     // Ocultar todos los elementos en depth=3 (limpiar el submenu)
     
     $("ul[data-depth='2']").on('mouseover', ' li', function() {
+    $(document).on('mouseover', "ul[data-depth='2'] li", function() {
         // Ocultar todos los elementos en depth=3 (limpiar el submenu)
+        $("ul[data-depth='2']").find("ul[data-depth='3']").parent().removeClass('show');
         $("ul[data-depth='2']").find("ul[data-depth='3']").parent().addClass('hidden');
         $("ul[data-depth='2']").find("ul[data-depth='3']").parent().attr('aria-expanded', false);
-        
         // Mostrar solo los children del elemento sobre el que estamos
         if(this.children[1]) {
             $(this).find("ul[data-depth='3']").parent().removeClass('hidden');
