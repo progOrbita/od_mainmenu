6ae37ff787439343736b0c20885579ba0ef4a767
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-11-02 18:43:43 +0100

    avoid removing sublevels on mobile

--- od_mainmenu.js
+++ od_mainmenu.js
@@ -10,23 +10,30 @@ $(function () {
 const categories_displayed = [1];
 var category;
 var request;
+var mobile = 0;
+var width = window.innerWidth;
+console.log(width);
+if(width < 768){
+    mobile = 1;
+}
+    if(!mobile){
+        $(document).on('mouseover', "ul[data-depth='2'] li", function() {
 
-    $(document).on('mouseover', "ul[data-depth='2'] li", function() {
-        $('#top-menu').find('.underline').removeClass('underline');
-        $(this).find('.item-header > a[data-depth="2"]').addClass('underline');
-        
-        // Hide all the elements in depth=3 (cleans the submenu)
-        $("ul[data-depth='2']").find("ul[data-depth='3']").parent().removeClass('show');
-        $("ul[data-depth='2']").find("ul[data-depth='3']").parent().addClass('hidden');
-        $("ul[data-depth='2']").find("ul[data-depth='3']").parent().attr('aria-expanded', false);
-        // Shows only the children of the element where we stood
-        if(this.children[1]) {
-            $(this).find("ul[data-depth='3']").parent().removeClass('hidden');
-            $(this).find("ul[data-depth='3']").parent().attr('aria-expanded', true);
-        }
-    });
+            //Add an underline to the submenu selected
+            $('#top-menu').find('.underline').removeClass('underline');
+            $(this).find('.item-header > a[data-depth="2"]').addClass('underline');
 
-    $(document).on( 'mouseenter','.category', function(){
+            // Hide all the elements in depth=3 (cleans the submenu)
+            $("ul[data-depth='2']").find("ul[data-depth='3']").parent().removeClass('show');
+            $("ul[data-depth='2']").find("ul[data-depth='3']").parent().addClass('hidden');
+            $("ul[data-depth='2']").find("ul[data-depth='3']").parent().attr('aria-expanded', false);
+            // Shows only the children of the element where we stood
+            if(this.children[1]) {
+                $(this).find("ul[data-depth='3']").parent().removeClass('hidden');
+                $(this).find("ul[data-depth='3']").parent().attr('aria-expanded', true);
+            }
+        });
+    }
 
         let selected_cat = $(this);
         let id_category = parseInt(selected_cat.attr('id').match(/\d+/g)[0]);
